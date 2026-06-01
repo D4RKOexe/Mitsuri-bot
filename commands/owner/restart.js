5f81b710eaefe97f6a18754d1a93d4f4694e3b51
BrayanRK <brayangasca37@gmail.com>
2026-06-01 18:47:42 -0500

    Update bot

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,21 +1,24 @@
-import { execSync } from "child_process";
 import { reply } from "../../utils.js";
 
 export default {
   name: "reiniciar",
   aliases: ["restart", "rst"],
-  run: async (sock, msg, args, jid, isOwner) => {
+
+  async run(sock, msg, args, jid, isOwner) {
 
     if (!isOwner) {
       return reply(sock, jid, "❌ Solo el dueño puede usar este comando.", msg);
     }
 
-    await reply(sock, jid, "🔄 *Reiniciando bot...*", msg);
+    await reply(
+      sock,
+      jid,
+      "🔄 Reiniciando bot...",
+      msg
+    );
 
-    try {
-      execSync("pm2 restart bytebot", { stdio: "pipe" });
-    } catch (e) {
-      await reply(sock, jid, `❌ Error al reiniciar.\n🔎 ${e.message}`, msg);
-    }
-  },
+    setTimeout(() => {
+      process.exit(0);
+    }, 1000);
+  }
 };
