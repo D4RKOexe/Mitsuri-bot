eb6285ccb6b4d71212116c7129fa3787afa6daae
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-06-09 04:58:55 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const CONFIG = {
+  ownerNumber: "51913454004",
+  botName: "𝘮𝘪𝘵𝘴𝘶𝘳𝘪 𝘣𝘰𝘵❤️",
+  prefix: ".",
+  sessionDir: "./session",
+  frase: "𝘓𝘢 𝘭𝘭𝘢𝘮𝘢 𝘲𝘶𝘦 𝘯𝘶𝘯𝘤𝘢 𝘴𝘦 𝘢𝘱𝘢𝘨𝘢 ❤️🔥",
+};
+
+export const TEMP_DIR = process.cwd() + "/temp";
