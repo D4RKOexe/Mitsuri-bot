668546a3297369b1929cafc96e25aede6ddf7785
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-06-09 20:30:36 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const CONFIG = {
+  ownerNumber: "51913454004",
+  ownerName: "GR",
+  botName: "DARKO BOT",
+  sessionDir: "./session",
+  frase: "el mejor bot de todo latam 🔥",
+};
+
+export const TEMP_DIR = process.cwd() + "/temp";
